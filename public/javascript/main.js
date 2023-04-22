de0612ae3d4a40bd366be3b3c490074accce3db1
Dan <daniel.pettitt.dev@gmail.com>
2023-04-22 12:00:00 +0100

    tidy up before templating

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,3 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
     contentDiv.innerHTML = renderedTemplate;
     // document.body.innerHTML = templateScript(obj);
   });
+
+  
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      $('.slide-in-section').each(function() {
+        var position = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > position - windowHeight + 100) {
+          $(this).addClass('is-visible');
+        }
+      });
+    });
+  });
+  
