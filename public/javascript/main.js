0547e2275762a37af02c2fb0598e11d64e1912e7
Dan <daniel.pettitt.dev@gmail.com>
2023-04-24 15:52:59 +0100

    used html templating for header and footer. started contact page

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let script1 = document.querySelector('#handlebars1').innerHTML;
-    let contentDiv = document.querySelector('.mybox2');
-
-    let obj = {
-      "name": "<h2>Dan</h2>",
-      "date": "<i>2020-12-10</i>",
-    };
-  
-    let templateScript = Handlebars.compile(script1); // returns a function
-    let renderedTemplate = templateScript(obj);
-
-    contentDiv.innerHTML = renderedTemplate;
-    // document.body.innerHTML = templateScript(obj);
-  });
-
-  
-
   $(document).ready(function() {
     $(window).scroll(function() {
       $('.slide-in-section').each(function() {
