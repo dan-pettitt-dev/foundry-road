96b3352b01a0510def0771c27e41f4482374fcfd
Dan <daniel.pettitt.dev@gmail.com>
2023-04-25 13:48:07 +0100

    refactored templating to use jquery

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,13 +1,20 @@
-  $(document).ready(function() {
-    $(window).scroll(function() {
-      $('.slide-in-section').each(function() {
-        var position = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > position - windowHeight + 100) {
-          $(this).addClass('is-visible');
-        }
-      });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".slide-in-section").each(function () {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 100) {
+        $(this).addClass("is-visible");
+      }
     });
   });
-  
+});
+
+$(function () {
+  $('#header').load("components/header.html")
+})
+
+$(function () {
+  $('#footer').load("components/footer.html")
+})
